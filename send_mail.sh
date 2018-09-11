9f1d3f8b829a2e1f5648c69dcff674ad150a9c5c
kdharsee <komail.dharsee@gmail.com>
2018-09-10 23:06:44 -0400

    Updating mailling script functionality

--- send_mail.sh
+++ send_mail.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 # vim ft=sh
 
-balance=`echo "($2 + 100) / 100" | bc`
-contactor=$( cat contactor.txt )
-#'Komail(kdharsee@cs.rochester.edu)'
+real_balance=`echo "scale=2; ($2 + 100) / 100" | bc`
+round_balance=`echo "($2 + 100) / 100" | bc`
 
-if [ $balance -eq 0 ]
+if [ $round_balance -eq 0 ]
 then
     :
-elif [ $balance -gt 0 ]
+elif [ $round_balance -gt 0 ]
 then
-    echo "Your snack bar balance is $balance. Thanks for having a positive credit!" | mail -s "Snack Bar Balance" $1
+	set -x
+    echo "Your snack bar balance is $real_balance. Thanks for having a positive credit!" | mailx -s "Snack Bar Balance" $1
+	{ set +x; } 2>/dev/null
 else
-    echo "Your snack bar balance is $balance. Please send the money through Google Pay to urcs.snackbar@gmail.com" | mail -s "Snack Bar Balance" $1
+	set -x
+    echo "Your snack bar balance is $real_balance. Please send the money through Google Pay to urcs.snackbar@gmailx.com" | mailx -s "Snack Bar Balance" $1
+	{ set +x; } 2>/dev/null
 fi
