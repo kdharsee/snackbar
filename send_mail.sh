ffb7804768be1ab886eee109f6899d380e5d2931
kdharsee <komail.dharsee@gmail.com>
2018-09-11 14:25:09 -0400

    Updated mailing headers and content

--- send_mail.sh
+++ send_mail.sh
@@ -10,10 +10,10 @@ then
 elif [ $round_balance -gt 0 ]
 then
 	set -x
-    echo "Your snack bar balance is $real_balance. Thanks for having a positive credit!" | mailx -s "Snack Bar Balance" $1
+    echo "Your snack bar balance is \$$real_balance. Thanks for having a positive credit!" | mailx -r "URCS Snack Bar <kdharsee@cs.rochester.edu>" -s "Snack Bar Balance" $1 
 	{ set +x; } 2>/dev/null
 else
 	set -x
-    echo "Your snack bar balance is $real_balance. Please send the money through Google Pay to urcs.snackbar@gmail.com" | mailx -s "Snack Bar Balance" $1
+    echo "Your snack bar balance is \$$real_balance. Please send the money through Google Pay to urcs.snackbar@gmail.com" | mailx -r "URCS Snack Bar <kdharsee@cs.rochester.edu>" -s "Snack Bar Balance" $1 
 	{ set +x; } 2>/dev/null
 fi
