57b6b3beda4b8f7c1c5280ee962f62e853555456
kdharsee <komail.dharsee@gmail.com>
2018-11-08 17:45:34 -0500

    Updated message

--- send_mail.sh
+++ send_mail.sh
@@ -14,6 +14,6 @@ then
 	{ set +x; } 2>/dev/null
 else
 	set -x
-    echo "Your snack bar balance is \$$real_balance. Please send the money through Google Pay to urcs.snackbar@gmail.com" | mailx -r "URCS Snack Bar <kdharsee@cs.rochester.edu>" -s "Snack Bar Balance" $1 
+    echo "Your snack bar balance is \$$real_balance. With your Snackbar username as a note, send the money through Google Pay to urcs.snackbar@gmail.com. If your username doesn't appear with the payment, it will not be processed." | mailx -r "URCS Snack Bar <kdharsee@cs.rochester.edu>" -s "Snack Bar Balance" $1 
 	{ set +x; } 2>/dev/null
 fi
